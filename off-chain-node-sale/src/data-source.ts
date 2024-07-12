43c03cb2e0453c6c66bf1c12c894518f1f7aff25
Dev Fred <manfredtochukwu362@gmail.com>
2024-07-12 22:48:31 +0100

    "Updated @types/node version, added TypeORM DataSource configuration and imported reflect-metadata in data-source.ts"

--- off-chain-node-sale/src/data-source.ts
+++ off-chain-node-sale/src/data-source.ts
@@ -1,2 +1,20 @@
-'use strict'
+import "reflect-metadata"
+import { DataSource } from "typeorm"
 
+const database = process.env.POSTGRESQL_DB
+const username = process.env.POSTGRESQL_USERNAME
+const password = process.env.POSTGRESQL_PASSWORD
+
+export const dataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username,
+    password,
+    database,
+    synchronize: true,
+    logging: false,
+    entities: ['./entity/**.ts'],
+    migrations: ['./migration/**.ts'],
+    subscribers: [],
+})
